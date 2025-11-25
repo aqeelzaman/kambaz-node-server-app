3f222256aa9aeae4058b4decf9dbb9f01ef58569
aqeelzaman <aqeelzaman8@gmail.com>
2025-11-25 16:33:21 -0500

    a6 database config

--- index.js
+++ index.js
@@ -10,32 +10,38 @@ import session from "express-session";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING)
 
 const app = express()
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL || "http://localhost:3000",
 }));
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kambaz",
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.SERVER_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
         secure: true,
         domain: process.env.SERVER_URL,
-    };
+    }
 }
-app.use(session(sessionOptions));
-app.use(express.json());
-UserRoutes(app, db);
-CourseRoutes(app, db);
-ModulesRoutes(app, db);
-AssignmentsRoutes(app, db);
-EnrollmentsRoutes(app, db);
-Hello(app);
-Lab5(app);
+app.use(session(sessionOptions))
+app.use(express.json())
+UserRoutes(app, db)
+CourseRoutes(app, db)
+ModulesRoutes(app, db)
+AssignmentsRoutes(app, db)
+EnrollmentsRoutes(app, db)
+Hello(app)
+Lab5(app)
 app.listen(process.env.PORT || 4000)
